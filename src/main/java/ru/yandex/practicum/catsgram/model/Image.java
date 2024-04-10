a107136cef1c60fa155cb9fe2b5a876f229c6ab2
Shiryaev Pavel <pashizh@yandex.ru>
2024-04-11 01:11:35 +0400

    add: new features

--- src/main/java/ru/yandex/practicum/catsgram/model/Image.java
+++ src/main/java/ru/yandex/practicum/catsgram/model/Image.java
@@ -1,7 +1,11 @@
 package ru.yandex.practicum.catsgram.model;
 
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @EqualsAndHashCode(of = {"id"})
 public class Image {
 
