44b2c530e061d7115247125e9ce429e7d0a65de1
shiryaev <shiryaev@lanit.ru>
2024-05-30 14:14:52 +0400

    add: data

--- src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -2,17 +2,13 @@ package ru.yandex.practicum.catsgram.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.catsgram.dto.NewUserRequest;
+import ru.yandex.practicum.catsgram.dto.UpdateUserRequest;
 import ru.yandex.practicum.catsgram.dto.UserDto;
-import ru.yandex.practicum.catsgram.mapper.UserMapper;
-import ru.yandex.practicum.catsgram.model.User;
 import ru.yandex.practicum.catsgram.service.UserService;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,10 +16,26 @@ import java.util.stream.Collectors;
 public class UserController {
     private final UserService userService;
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDto createUser(@RequestBody NewUserRequest userRequest) {
+        return userService.createUser(userRequest);
+    }
+
+    @PutMapping("/{userId}")
+    public UserDto updateUser(@PathVariable("userId") long userId, @RequestBody UpdateUserRequest request) {
+        return userService.updateUser(userId, request);
+    }
+
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<UserDto> getUsers() {
-        List<User> users = userService.getUsers();
-        return users.stream().map(UserMapper::mapToUserDto).collect(Collectors.toList());
+        return userService.getUsers();
+    }
+
+    @GetMapping("/{userId}")
+    @ResponseStatus(HttpStatus.OK)
+    public UserDto getUserById(@PathVariable("userId") long userId) {
+        return userService.getUserById(userId);
     }
 }
